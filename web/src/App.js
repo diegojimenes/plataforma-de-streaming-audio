51e3e1ab162f9e0ea555a3aa31580fa18ca37423
Diego jimenes <lligaskate@gmail.com>
2020-06-27 13:28:06 -0300

    Router dom

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// rotas
+import Login from './screens/login'
+import Start from './screens/start'
+import Player from './screens/player'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/player">Player</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/player">
+            <Player />
+          </Route>
+          <Route path="/">
+            <Start />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
